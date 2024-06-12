ddec1d671d2e2fc37271f4760b68527ca80d8ae7
정승민 <67104714+Codemaniac7@users.noreply.github.com>
2024-06-13 04:31:25 +0900

    Add files via upload

---
+++ src/components/MainComp.vue
@@ -0,0 +1,11 @@
+<script setup>
+import SearchBar from "./SearchBar.vue";
+import WeatherInfo from "./WeatherInfo.vue";
+</script>
+
+<template>
+  <div class="container">
+    <SearchBar />
+    <WeatherInfo />
+  </div>
+</template>
